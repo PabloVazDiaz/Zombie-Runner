49615e828e9aa0480dcd7d6a88b8f4ad6a47f5ce
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-16 11:03:07 +0200

    Raycast Shooting, shoot and hit particles

--- ZombieRunner/Assets/Script/EnemyAI.cs
+++ ZombieRunner/Assets/Script/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,7 +8,12 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +24,44 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
         
+    }
+
+    private void EngageTarget()
+    {
+        
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if(distanceToTarget<= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        print("Attacking");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
---
+++ ZombieRunner/Assets/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayMuzzleEffect();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleEffect()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            GameObject go = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(go, 0.1f);
+            print(hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target != null)
+                target.Damage(damage);
+        }
+    }
+}
