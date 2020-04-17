ed8d28023d1e0602270321b8b0bb477d27ef558d
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-17 10:28:56 +0200

    Ammo PickUps

---
+++ ZombieRunner/Assets/AmmoPickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickUp : MonoBehaviour
+{
+
+    [SerializeField] AmmoType ammoType;
+    [SerializeField] int amount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<Ammo>().IncreaseCurrentAmmo(ammoType, amount);
+            Destroy(this.gameObject);
+        }
+    }
+}
